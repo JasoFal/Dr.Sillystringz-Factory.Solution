ed0427072e92b112bf0c57c293107e49b8a7a89a
Jason Falk <jasonfalk9@gmail.com>
2024-04-18 15:37:44 -0700

    Add machine create route.

--- Factory/Controllers/MachinesController.cs
+++ Factory/Controllers/MachinesController.cs
@@ -10,16 +10,29 @@ namespace Factory.Controllers
   public class MachinesController : Controller
   {
     private readonly FactoryContext _db;
-  }
+  
+    public MachinesController(FactoryContext db)
+    {
+      _db = db;
+    }
 
-  public MachinesController(FactoryContext db)
-  {
-    _db = db;
-  }
+    public ActionResult Index()
+    {
+      List<Machine> model = _db.Machines.ToList();
+      return View(model);
+    }
 
-  public ActionResult Index()
-  {
-    List<Machine> model = _db.Machines.ToList();
-    return View(model);
+    public ActionResult Create()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Machine machine)
+    {
+      _db.Machines.Add(machine);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
   }
 }
