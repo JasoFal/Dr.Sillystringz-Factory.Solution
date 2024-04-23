002649a9d126b280e8b80fbf7af6f4ac38bbe5f0
Jason Falk <jasonfalk9@gmail.com>
2024-04-23 10:24:25 -0700

    Removed "!" from error messages.

--- Factory/Models/Engineer.cs
+++ Factory/Models/Engineer.cs
@@ -8,9 +8,9 @@ namespace Factory.Models
   public class Engineer
   {
     public int EngineerId { get; set; }
-    [Required(ErrorMessage = "Engineer Name Can't Be Empty!")]
+    [Required(ErrorMessage = "Engineer Name Can't Be Empty")]
     public string EngineerName { get; set; }
-    [Required(ErrorMessage = "Job Title Can't Be Empty!")]
+    [Required(ErrorMessage = "Job Title Can't Be Empty")]
     public string JobTitle { get; set; }
     public List<EngineerMachine> JoinEntities { get; }
   }
